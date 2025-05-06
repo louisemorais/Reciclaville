9120434c6b131bf736a655400074611fd488f878
Louise Morais <louisemorais3@gmail.com>
2025-05-06 16:31:10 -0300

    init: inicializando repositório Git do projeto final

---
+++ src/main/java/br/senai/lab364/futurodev/reciclaville/ReciclavilleApplication.java
@@ -0,0 +1,13 @@
+package br.senai.lab364.futurodev.reciclaville;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclavilleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReciclavilleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/senai/lab364/futurodev/reciclaville/ReciclavilleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.lab364.futurodev.reciclaville;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclavilleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
