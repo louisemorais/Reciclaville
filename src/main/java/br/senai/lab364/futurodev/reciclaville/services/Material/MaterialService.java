c9096ec75e992b695b20ef113b6c56bea6fc46b4
Louise Morais <louisemorais3@gmail.com>
2025-05-07 10:25:58 -0300

    feat: criação do service de material

---
+++ src/main/java/br/senai/lab364/futurodev/reciclaville/services/Material/MaterialService.java
@@ -0,0 +1,49 @@
+package br.senai.lab364.futurodev.reciclaville.services.Material;
+
+import br.senai.lab364.futurodev.reciclaville.dtos.MaterialsDTO.RequestMaterialDTO;
+import br.senai.lab364.futurodev.reciclaville.dtos.MaterialsDTO.ResponseMaterialDTO;
+import br.senai.lab364.futurodev.reciclaville.mappers.MapperMaterial;
+import br.senai.lab364.futurodev.reciclaville.models.Material;
+import br.senai.lab364.futurodev.reciclaville.repositories.MaterialRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class MaterialService implements MaterialServiceInterf{
+
+    private final MaterialRepository repository;
+    private final MapperMaterial materialMapper;
+
+    @Override
+    public List<ResponseMaterialDTO> findAll() {
+        return materialMapper.toResponseDTO(repository.findAll());
+    }
+
+    @Override
+    public ResponseMaterialDTO findById(Long id) {
+        Material entity = repository.findById(id).orElse(null);
+        return materialMapper.toResponseDTO(entity);
+    }
+
+    @Override
+    public ResponseMaterialDTO creates(RequestMaterialDTO dto) {
+        Material material = materialMapper.toEntity(new Material(), dto);
+        return materialMapper.toResponseDTO(repository.save(material));
+    }
+
+    @Override
+    public ResponseMaterialDTO update(Long id, RequestMaterialDTO dto) {
+        Material material = repository.findById(id).orElse(null);
+        materialMapper.toEntity(material, dto);
+        return materialMapper.toResponseDTO(repository.save(material));
+    }
+
+    @Override
+    public void delete(Long id) {
+        Material material = repository.findById(id).orElse(null);
+        repository.delete(material);
+    }
+}
---
+++ src/main/java/br/senai/lab364/futurodev/reciclaville/services/Material/MaterialServiceInterf.java
@@ -0,0 +1,14 @@
+package br.senai.lab364.futurodev.reciclaville.services.Material;
+
+import br.senai.lab364.futurodev.reciclaville.dtos.MaterialsDTO.RequestMaterialDTO;
+import br.senai.lab364.futurodev.reciclaville.dtos.MaterialsDTO.ResponseMaterialDTO;
+
+import java.util.List;
+
+public interface MaterialServiceInterf {
+    List<ResponseMaterialDTO> findAll();
+    ResponseMaterialDTO findById(Long id);
+    ResponseMaterialDTO creates(RequestMaterialDTO dto);
+    ResponseMaterialDTO update(Long id, RequestMaterialDTO dto);
+    void delete(Long id);
+}
