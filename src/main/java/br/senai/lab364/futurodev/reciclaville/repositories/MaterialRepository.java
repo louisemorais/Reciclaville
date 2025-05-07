381f8d1a09226c1fdd21d5fbedc495150b54afe2
Louise Morais <louisemorais3@gmail.com>
2025-05-06 22:24:56 -0300

    feat: adicionando anotação de repository

--- src/main/java/br/senai/lab364/futurodev/reciclaville/repositories/MaterialRepository.java
+++ src/main/java/br/senai/lab364/futurodev/reciclaville/repositories/MaterialRepository.java
@@ -2,6 +2,8 @@ package br.senai.lab364.futurodev.reciclaville.repositories;
 
 import br.senai.lab364.futurodev.reciclaville.models.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MaterialRepository extends JpaRepository<Material, Integer> {
 }
